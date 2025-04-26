850273d3104759bc37d02e88c4082993c0b95247
DCworld007 <dan.dc.chu@gmail.com>
2025-04-26 01:37:14 -0400

    Fix MapComponent cleanup function

--- src/components/Map/MapComponent.tsx
+++ src/components/Map/MapComponent.tsx
@@ -1,123 +1,44 @@
-import { useEffect } from 'react';
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-import { Location, MapProps } from './index';
+import React, { useEffect, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { Location } from '@/types/location';
 
-const DEFAULT_CENTER = {
-  lat: 37.7749,
-  lng: -122.4194,
-};
+interface MapProps {
+  location: Location;
+}
 
-// Initialize icons
-const markerIcon = L.icon({
-  iconUrl: '/marker-icon-2x.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41]
-});
-
-const currentLocationIcon = L.icon({
-  iconUrl: '/current-location-marker.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41]
-});
-
-const BoundsUpdater = ({ locations, currentLocation }: { locations: Location[], currentLocation?: Location }) => {
-  const map = useMap();
-  
-  useEffect(() => {
-    const mapInstance = map;
-    if (!mapInstance) return;
-    
-    const points = [...locations];
-    if (currentLocation) {
-      points.push(currentLocation);
-    }
-
-    if (points.length > 0) {
-      const bounds = L.latLngBounds(points.map(loc => [loc.lat, loc.lng]));
-      mapInstance.fitBounds(bounds, { padding: [50, 50] });
-    }
-  }, [map, locations, currentLocation]);
-
-  return null;
-};
-
-const ClickHandler = ({ onMapClick, enableClickSelection }: { onMapClick?: (location: Location) => void, enableClickSelection?: boolean }) => {
-  const map = useMap();
+const MapComponent: React.FC<MapProps> = ({ location }) => {
+  const mapContainer = useRef<HTMLDivElement | null>(null);
+  const mapRef = useRef<mapboxgl.Map | null>(null);
+  const markerRef = useRef<mapboxgl.Marker | null>(null);
 
   useEffect(() => {
-    if (!enableClickSelection || !onMapClick || !map) return;
+    if (!mapRef.current && mapContainer.current) {
+      const map = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [location.lng, location.lat],
+        zoom: 12
+      });
 
-    const handleClick = async (e: L.LeafletMouseEvent) => {
-      try {
-        const response = await fetch(
-          `https://nominatim.openstreetmap.org/reverse?format=json&lat=${e.latlng.lat}&lon=${e.latlng.lng}`
-        );
-        const data = await response.json();
-        
-        if (data && data.display_name) {
-          onMapClick({
-            lat: e.latlng.lat,
-            lng: e.latlng.lng,
-            address: data.display_name,
-          });
-        }
-      } catch (error) {
-        console.error('[ClickHandler] Error fetching location:', error);
+      mapRef.current = map;
+
+      const marker = new mapboxgl.Marker()
+        .setLngLat([location.lng, location.lat])
+        .addTo(map);
+
+      markerRef.current = marker;
+    }
+
+    return () => {
+      if (mapRef.current) {
+        mapRef.current.remove();
+        mapRef.current = null;
       }
     };
+  }, [location]);
 
-    map.on('click', handleClick);
-    return () => map.off('click', handleClick);
-  }, [map, enableClickSelection, onMapClick]);
-
-  return null;
-};
-
-const MapComponent = ({ locations = [], currentLocation, onMapClick, enableClickSelection = false }: MapProps) => {
-  const center = currentLocation 
-    ? [currentLocation.lat, currentLocation.lng] 
-    : [DEFAULT_CENTER.lat, DEFAULT_CENTER.lng];
-
-  return (
-    <div style={{ height: '100%', width: '100%' }}>
-      <MapContainer
-        center={center as [number, number]}
-        zoom={13}
-        scrollWheelZoom={true}
-        style={{ height: '100%', width: '100%' }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {locations.map((location, index) => (
-          <Marker
-            key={`${location.lat}-${location.lng}-${index}`}
-            position={[location.lat, location.lng]}
-            icon={markerIcon}
-          >
-            <Popup>{location.address}</Popup>
-          </Marker>
-        ))}
-        {currentLocation && (
-          <Marker
-            position={[currentLocation.lat, currentLocation.lng]}
-            icon={currentLocationIcon}
-          >
-            <Popup>Current Location</Popup>
-          </Marker>
-        )}
-        <BoundsUpdater locations={locations} currentLocation={currentLocation} />
-        <ClickHandler onMapClick={onMapClick} enableClickSelection={enableClickSelection} />
-      </MapContainer>
-    </div>
-  );
+  return <div ref={mapContainer} style={{ width: '100%', height: '400px' }} />;
 };
 
 export default MapComponent; 
