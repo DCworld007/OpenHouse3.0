dcb907d886efa7c0cb3691dd74bdfd58331883ed
DCworld007 <dan.dc.chu@gmail.com>
2025-04-26 01:58:58 -0400

    Optimize bundle size and update Cloudflare Pages configuration

--- next.config.js
+++ next.config.js
@@ -1,12 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
+        hostname: '**',
       },
     ],
+  },
+  // Optimize for Cloudflare Pages
+  output: 'standalone',
+  experimental: {
+    optimizePackageImports: ['@heroicons/react'],
+  },
+  webpack: (config, { isServer }) => {
+    // Optimize bundle size
+    config.optimization = {
+      ...config.optimization,
+      minimize: true,
+    }
+    return config
   },
 }
 
