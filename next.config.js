5696af586f065cb8cfb016dddc165340d31cc73c
DCworld007 <dan.dc.chu@gmail.com>
2025-04-26 08:27:09 -0400

    Simplify Next.js configuration for Cloudflare Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: true
-})
-const path = require('path')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
@@ -16,75 +9,10 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config, { dev, isServer }) => {
-    // Only optimize in production builds
-    if (!dev) {
-      config.cache = {
-        type: 'filesystem',
-        // Limit cache size
-        maxMemoryGenerations: 1,
-        // Use absolute path for cache directory
-        cacheDirectory: path.resolve('.next/cache/webpack'),
-        // Clean up old caches
-        compression: 'gzip',
-        // Only cache production files
-        name: isServer ? 'server' : 'client',
-        // Cache version - bump this if you change webpack config
-        version: '1.0.0'
-      }
-
-      if (!isServer) {
-        // Client-side optimizations
-        config.optimization = {
-          ...config.optimization,
-          // Enable tree shaking
-          usedExports: true,
-          // Minimize output
-          minimize: true,
-          // Split chunks efficiently
-          splitChunks: {
-            chunks: 'all',
-            minSize: 20000,
-            maxSize: 90000, // Reduced chunk size for better caching
-            cacheGroups: {
-              default: false,
-              vendors: false,
-              framework: {
-                name: 'framework',
-                test: /[\\/]node_modules[\\/](react|react-dom|next)[\\/]/,
-                priority: 40,
-                enforce: true,
-                chunks: 'all'
-              },
-              lib: {
-                test: /[\\/]node_modules[\\/](leaflet|mapbox-gl|framer-motion|@dnd-kit)[\\/]/,
-                name: (module) => {
-                  const match = module.context.match(/[\\/]node_modules[\\/](.*?)(?:[\\/]|$)/);
-                  return `lib-${match[1].replace('@', '')}`; 
-                },
-                priority: 30,
-                minChunks: 1,
-                reuseExistingChunk: true,
-                chunks: 'all'
-              },
-              commons: {
-                name: 'commons',
-                minChunks: 2,
-                priority: 20,
-                reuseExistingChunk: true,
-                chunks: 'all'
-              }
-            }
-          }
-        }
-      }
-    }
-    return config
-  },
   experimental: {
-    optimizeCss: true, // This will inline critical CSS
+    optimizeCss: true,
     optimizePackageImports: ['@heroicons/react', '@headlessui/react'],
   },
 }
 
-module.exports = withBundleAnalyzer(nextConfig) 
+module.exports = nextConfig 
